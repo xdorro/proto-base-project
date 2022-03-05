9d2400010a1b6e33fcf667fb7c06a999534fccf1
Tuan Anh Nguyen Manh <anhng.dev@gmail.com>
2022-03-05 13:23:31 +0700

    Update user proto

--- protos/v1/user/user.pb.gw.go
+++ protos/v1/user/user.pb.gw.go
@@ -309,7 +309,7 @@ func RegisterUserServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/userpb.UserService/FindUserByID", runtime.WithHTTPPathPattern("/api/v1/users/{id=*}"))
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/userpb.UserService/FindUserByID", runtime.WithHTTPPathPattern("/api/v1/users/{id}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -355,7 +355,7 @@ func RegisterUserServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/userpb.UserService/UpdateUser", runtime.WithHTTPPathPattern("/api/v1/users/{id=*}"))
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/userpb.UserService/UpdateUser", runtime.WithHTTPPathPattern("/api/v1/users/{id}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -378,7 +378,7 @@ func RegisterUserServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/userpb.UserService/DeleteUser", runtime.WithHTTPPathPattern("/api/v1/users/{id=*}"))
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/userpb.UserService/DeleteUser", runtime.WithHTTPPathPattern("/api/v1/users/{id}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -460,7 +460,7 @@ func RegisterUserServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/userpb.UserService/FindUserByID", runtime.WithHTTPPathPattern("/api/v1/users/{id=*}"))
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/userpb.UserService/FindUserByID", runtime.WithHTTPPathPattern("/api/v1/users/{id}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -500,7 +500,7 @@ func RegisterUserServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/userpb.UserService/UpdateUser", runtime.WithHTTPPathPattern("/api/v1/users/{id=*}"))
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/userpb.UserService/UpdateUser", runtime.WithHTTPPathPattern("/api/v1/users/{id}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -520,7 +520,7 @@ func RegisterUserServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/userpb.UserService/DeleteUser", runtime.WithHTTPPathPattern("/api/v1/users/{id=*}"))
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/userpb.UserService/DeleteUser", runtime.WithHTTPPathPattern("/api/v1/users/{id}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
